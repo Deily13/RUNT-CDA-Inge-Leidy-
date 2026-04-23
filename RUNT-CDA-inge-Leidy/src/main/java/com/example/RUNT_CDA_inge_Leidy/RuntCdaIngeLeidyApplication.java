e2c4beadd22d79e631f6a4aea4fde8a77020d60f
Deily13 <quinceyton.17@gamil.com>
2026-04-23 15:42:20 -0500

    proyecto Spring

---
+++ RUNT-CDA-inge-Leidy/src/main/java/com/example/RUNT_CDA_inge_Leidy/RuntCdaIngeLeidyApplication.java
@@ -0,0 +1,13 @@
+package com.example.RUNT_CDA_inge_Leidy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuntCdaIngeLeidyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RuntCdaIngeLeidyApplication.class, args);
+	}
+
+}
---
+++ RUNT-CDA-inge-Leidy/src/test/java/com/example/RUNT_CDA_inge_Leidy/RuntCdaIngeLeidyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RUNT_CDA_inge_Leidy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuntCdaIngeLeidyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
