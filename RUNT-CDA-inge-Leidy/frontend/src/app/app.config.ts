95baeba7a135c7f2da4245de31096f472f115dd9
Deily13 <quinceyton.17@gamil.com>
2026-06-02 16:00:09 -0500

    creacion vista login y base

--- RUNT-CDA-inge-Leidy/frontend/src/app/app.config.ts
+++ RUNT-CDA-inge-Leidy/frontend/src/app/app.config.ts
@@ -1,10 +1,12 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes)
   ]
--- RUNT-CDA-inge-Leidy/frontend/src/app/app.routes.ts
+++ RUNT-CDA-inge-Leidy/frontend/src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {
+        path: 'login',
+        loadComponent: () =>
+            import('./components/login/login.component').then(m => m.LoginComponent)
+    },
+    {
+        path: 'base',
+        loadComponent: () =>
+            import('./components/base/base.component').then(m => m.Base),
+        children: [
+            {
+                path: 'tabla-registro',
+                loadComponent: () =>
+                    import('./components/tabla-registro/tabla-registro.component')
+                        .then(m => m.TablaRegistroComponent)
+            }
+        ]
+    },
+    { path: '', redirectTo: 'login', pathMatch: 'full' }
+];
