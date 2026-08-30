e67e69f353d5b7a5bbe6ceee0222332fe6e136e1
Deily13 <quinceyton.17@gamil.com>
2026-08-29 23:18:40 -0500

    nuevo componente para gestionar reporte de vehiculo

---
+++ RUNT-CDA-inge-Leidy/frontend/src/app/components/gestion-reporte/gestion-reporte.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gestion-reporte',
+  imports: [],
+  templateUrl: './gestion-reporte.component.html',
+  styleUrl: './gestion-reporte.component.css',
+})
+export class GestionReporte {}
